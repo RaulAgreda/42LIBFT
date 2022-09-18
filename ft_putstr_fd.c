f6175feb34be313474866f989235544a2230d46c
Raul Agreda Garcia <ragreda-@c3r11s3.42madrid.com>
2022-09-18 17:38:06 +0200

    protegidos lstdelone y putstr

--- ft_putstr_fd.c
+++ ft_putstr_fd.c
@@ -6,7 +6,7 @@
 /*   By: ragreda- <ragreda-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/15 11:27:45 by ragreda-          #+#    #+#             */
-/*   Updated: 2022/09/15 11:29:56 by ragreda-         ###   ########.fr       */
+/*   Updated: 2022/09/18 17:35:54 by ragreda-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,6 +14,8 @@
 
 void	ft_putstr_fd(char *s, int fd)
 {
+	if (!s)
+		return ;
 	while (*s != 0)
 	{
 		write(fd, s, 1);
--- ft_strmapi.c
+++ ft_strmapi.c
@@ -6,7 +6,7 @@
 /*   By: ragreda- <ragreda-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/15 11:14:25 by ragreda-          #+#    #+#             */
-/*   Updated: 2022/09/15 11:19:48 by ragreda-         ###   ########.fr       */
+/*   Updated: 2022/09/18 17:35:01 by ragreda-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,6 +18,8 @@ char	*ft_strmapi(char const *s, char (*f)(unsigned int, char))
 	unsigned int	i;
 	char			*ret;
 
+	if (!s)
+		return (0);
 	ret = malloc(ft_strlen(s) + 1);
 	if (ret == 0)
 		return (0);
