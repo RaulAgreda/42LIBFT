09711718777554f89023fb15cfba62dd0d46ba50
Raul Agreda Garcia <ragreda-@c3r11s3.42madrid.com>
2022-09-23 19:05:05 +0200

    calloc corregido bien, y Makefile no compila bien con lo del relink, ta feo hecho

--- ft_calloc.c
+++ ft_calloc.c
@@ -6,7 +6,7 @@
 /*   By: ragreda- <ragreda-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/13 20:16:13 by ragreda-          #+#    #+#             */
-/*   Updated: 2022/09/18 22:26:45 by ragreda-         ###   ########.fr       */
+/*   Updated: 2022/09/23 19:00:45 by ragreda-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,7 +20,7 @@ void	*ft_calloc(size_t nmemb, size_t size)
 	size_t			i;
 	unsigned char	*ret;
 
-	if (!nmemb || !size || nmemb > UINT_MAX / size)
+	if (nmemb != 0 && size > SIZE_MAX / nmemb)
 		return (0);
 	mem = nmemb * size;
 	ret = malloc(nmemb * size);
